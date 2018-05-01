e8af5e99bbeb5fe3b1e7864c1ea861f81ea5e621
Vlad Onofrei <vlad.onofrei96@gmail.com>
2018-05-01 02:05:03 +0100

    javascript exercises

---
+++ JAVASCRIPT/First_exercise/script.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var secondName = prompt("What is your second name?");
+var fullName = firstName + " " + secondName;
+var age = prompt("How old are you?");
+
+console.log("You full name is " + fullName + ".");
+console.log("You are " + age + " years old.");
---
+++ JAVASCRIPT/Second_exercise/script.js
@@ -0,0 +1,4 @@
+var age = prompt("What is your age?");
+var bisectYears = age / 4;
+
+alert(age + " years is roughly " + (age * 365 + bisectYears) + " days.");
