3d831028e11ecb8d77c98737ab6847699919c351
Vlad Onofrei <vlad.onofrei96@gmail.com>
2018-05-03 02:03:20 +0100

    dom selectors

---
+++ DOM/select.js
@@ -0,0 +1,5 @@
+// 4 differents ways to select the first <p> tag
+var firstMethod = document.getElementById("first");
+var secondMethod = document.getElementsByClassName("special")[0];
+var thirdMethod = document.querySelector("#first");
+var forthMethod = document.querySelectorAll(".special")[0];
